b1156740986bff43aff904618ab5ee1d81210a99
amine-haddad <seniortouco@outlook.fr>
2021-06-24 11:34:40 +0200

    Initial commit

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/programs", name="program_")
+ */
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"},requirements={"id"="\d+"})
+     * return Response
+     */
+    public function show(int $id ): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
