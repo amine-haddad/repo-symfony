8fbd178022dbd2c699cbca30cc08378471f01f08
amine-haddad <seniortouco@outlook.fr>
2021-09-10 19:13:29 +0200

    creation et gestion des utilisateurs ok login et logout ok

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker;
+
+
+class UserFixtures extends Fixture
+{
+    //private $passwordHasher;
+    private $passwordEncoder;
+    public function __construct( UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher =$passwordHasher;
+        //$this->passwordEncoder =$passwordEncoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $faker= Faker\Factory::create('fr_FR');
+        // Création d’un utilisateur de type “contributeur” (= auteur)
+        $contributor = new User() ;
+        $contributor->setFirstname($faker->firstName())
+        ->setLastname($faker->lastName())
+        ->setBirthdayDate($faker->dateTimeBetween('-30 years','-15 years'));
+        $contributor->setEmail('contributor@monsite.com');
+        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor->setPassword($this->passwordHasher->hashPassword(
+            $contributor,
+            'contributorpassword'
+        ));
+       
+        $manager->persist($contributor);
+        
+        // Création d’un utilisateur de type “administrateur”
+        $admin = new User();
+        $admin->setFirstname($faker->firstName())
+        ->setLastname($faker->lastName())
+        ->setBirthdayDate($faker->dateTimeBetween('-30 years','-15 years'));
+        $admin->setEmail('admin@monsite.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordHasher->hashPassword(
+            $admin,
+            'adminpassword'
+        ));
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
